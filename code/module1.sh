29283302e568d60927f23fefbffc7afb825d619a
ELatham <elatham19@gmail.com>
2017-03-03 09:53:59 -0500

    Create module1.sh

---
+++ code/module1.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+# the man command gives you a manual for a specific command
+$man echo
+# the help command gives you shortened instructions on the uses of a command
+$help echo
+# the apropose command gives you details about a command
+$apropose echo
