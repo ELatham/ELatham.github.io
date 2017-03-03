d3ad2c428f399fc00bac0dbe17d45dd9bf3be09e
ELatham <elatham19@gmail.com>
2017-03-03 09:59:11 -0500

    Update module1.sh

--- code/module1.sh
+++ code/module1.sh
@@ -3,5 +3,5 @@
 $man echo
 # the help command gives you shortened instructions on the uses of a command
 $help echo
-# the apropose command gives you details about a command
-$apropose echo
+# the apropos command gives you details about a command
+$apropos echo
